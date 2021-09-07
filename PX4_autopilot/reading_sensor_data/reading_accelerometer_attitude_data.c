b8d99767e9ff34afcf0eed629538692e5aa6bbd8
Pratik Prajapati <31645111+pratikiitgn@users.noreply.github.com>
2021-09-07 14:18:19 +0530

    Create reading_accelerometer_attitude_data.c

---
+++ PX4_autopilot/reading_sensor_data/reading_accelerometer_attitude_data.c
@@ -0,0 +1,81 @@
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/posix.h>
+#include <poll.h>
+#include <uORB/uORB.h>
+#include <uORB/topics/sensor_combined.h>
+#include <uORB/topics/vehicle_attitude.h>
+
+__EXPORT int reading_sensor_data_main(int argc, char *argv[]);
+
+
+int reading_sensor_data_main(int argc, char *argv[])
+{
+
+  PX4_INFO("Hello Sky!");
+
+    /* subscribe to sensor_combined topic */
+    int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
+    int vehicle_attitude_fd = orb_subscribe(ORB_ID(vehicle_attitude));
+
+    /* limit the update rate to 5 Hz */
+    orb_set_interval(sensor_sub_fd, 200);
+    orb_set_interval(vehicle_attitude_fd, 200);
+
+    /* one could wait for multiple topics with this technique, just using one here */
+    px4_pollfd_struct_t fds[] = {
+        { .fd = sensor_sub_fd,   .events = POLLIN },
+        { .fd = vehicle_attitude_fd,   .events = POLLIN },
+        /* there could be more file descriptors here, in the form like:
+         * { .fd = other_sub_fd,   .events = POLLIN },
+         */
+    };
+
+    int error_counter = 0;
+
+    for (int i = 0; i < 5; i++) {
+        /* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
+        int poll_ret = px4_poll(fds, 1, 1000);
+
+        /* handle the poll result */
+        if (poll_ret == 0) {
+            /* this means none of our providers is giving us data */
+            PX4_ERR("Got no data within a second");
+
+        } else if (poll_ret < 0) {
+            /* this is seriously bad - should be an emergency */
+            if (error_counter < 10 || error_counter % 50 == 0) {
+                /* use a counter to prevent flooding (and slowing us down) */
+                PX4_ERR("ERROR return value from poll(): %d", poll_ret);
+            }
+
+            error_counter++;
+
+        } else {
+
+            if (fds[0].revents & POLLIN) {
+                /* obtained data for the first file descriptor */
+                struct sensor_combined_s raw;
+                struct vehicle_attitude_s raw_;
+
+                /* copy sensors raw data into local buffer */
+                orb_copy(ORB_ID(sensor_combined), sensor_sub_fd, &raw);
+                PX4_INFO("Accelerometer:\t%8.4f\t%8.4f\t%8.4f",
+                     (double)raw.accelerometer_m_s2[0],
+                     (double)raw.accelerometer_m_s2[1],
+                     (double)raw.accelerometer_m_s2[2]);
+
+                orb_copy(ORB_ID(vehicle_attitude),vehicle_attitude_fd,&raw_);
+                PX4_INFO("Vehicle attitude:\t%8.4f\t%8.4f\t%8.4f\t%8.4f",
+                     (double)raw_.q[0],
+                     (double)raw_.q[1],
+                     (double)raw_.q[2],
+                     (double)raw_.q[3]);
+
+            }
+        }
+    }
+
+    PX4_INFO("exiting");
+
+    return 0;
+}
